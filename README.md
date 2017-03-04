8c32fd4406fbf9aad33cc921e4cdb8c14b34b8bf
Luke Segars <luke@lukesegars.com>
2017-03-04 14:13:38 -0500

    Installation and usage instructions as well as changing defaults.
    Value of --dir is process.cwd() by default.
    If --ttl is zero, disable expiration.

--- README.md
+++ README.md
@@ -4,3 +4,20 @@
 dropd runs a small server that makes it really easy to share files on a trusted network. Users drag and drop files, which then become available for a specified period of time to other users. Once the period has passed, the file is automatically deleted.
 
 No permanent state is maintained and only files that were created by the dropd server can be accessed and deleted.
+
+## Installation and usage
+
+```
+npm install --global dropd
+```
+
+Once installed, you can run dropd from any directory. By default it will store any uploaded files in the working directory.
+
+```
+Configuration options
+
+    --dir   Absolute path to the directory where dropped files should be stored
+            default=process.cwd()
+    --ttl   Seconds that uploaded files should exist or zero for forever
+            default=60
+```
--- dropd.js
+++ dropd.js
@@ -4,13 +4,13 @@ const Nes = require('nes');
 const fs = require('fs');
 const yargs = require('yargs')
     .usage('Usage: $0 --dir [string] --ttl [num]')
-    .default('dir', 'storage')
+    .default('dir', process.cwd())
     .default('ttl', 60)
     .argv;
 
 const Events = require('./events');
 const FILE_STORAGE_DIRECTORY = yargs.dir;
-const SECONDS_LIFETIME = yargs.ttl;
+const SECONDS_LIFETIME = parseInt(yargs.ttl) === 0 ? null : parseInt(yargs.ttl);
 
 let next_id = 0;
 
@@ -18,14 +18,19 @@ const noop = () => { };
 const log = msg => console.log(`[${new Date().toISOString()}] ${msg}`);
 
 log(`Config: Writing dropped files to public/${FILE_STORAGE_DIRECTORY}`);
-log(`Config: Deleting after ${SECONDS_LIFETIME} seconds`);
+if (SECONDS_LIFETIME) log(`Config: Deleting after ${SECONDS_LIFETIME} seconds`);
+else log(`Config: File deletion disabled`)
 
 function DropFile(raw) {
     this.id = next_id++;
     // The user-facing name of the file.
     this.name = raw.filename;
     // When the file should expire.
-    this.expires = new Date(Date.now() + (SECONDS_LIFETIME * 1000)).toISOString();
+    if (SECONDS_LIFETIME) {
+        this.expires = new Date(Date.now() + (SECONDS_LIFETIME * 1000)).toISOString();
+    } else {
+        this.expires = null;
+    }
     // Convertible into URL in frontend.
     this.download_path = `${FILE_STORAGE_DIRECTORY}/${raw.filename}`;
 
@@ -103,14 +108,16 @@ server.start(err => {
 
     setInterval(() => {
         for (let i = 0; i < active_files.length; i++) {
-            if (Date.now() > Date.parse(active_files[i].expires)) {
-                log(`Deleting file '${active_files[i].name}'`);
+            const current = active_files[i];
+
+            if (current.expires && Date.now() > Date.parse(current.expires)) {
+                log(`Deleting file '${current.name}'`);
                 // Delete the file.
                 try {
-                    fs.unlink(active_files[i].download_path, noop);
+                    fs.unlink(current.download_path, noop);
                 } catch (e) { }
                 // Let all clients know.
-                server.publish('/files/updates', Events.Expire(active_files[i]));
+                server.publish('/files/updates', Events.Expire(current));
                 // Remove it from the active files array.
                 active_files.splice(i, 1);
                 i--;
